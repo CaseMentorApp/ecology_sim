4ae0f2a6fc5cb352654d2b75ffd72321d1658a37
torhadas1 <110469820+torhadas1@users.noreply.github.com>
2023-04-30 11:20:45 +0300

    Add files via upload

--- Simulation1.py
+++ Simulation1.py
@@ -41,7 +41,7 @@ st.set_page_config(layout="wide", page_title="Ecosystem_Management_Simulation")
 hide_streamlit_style = """
             <style>
             footer {visibility: hidden;}
-            #MainMenu {visibility: none;}
+            #MainMenu {display: none;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
@@ -49,7 +49,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 hide_streamlit_badge = """
                         <style>
-                        .viewerBadge_link__1S137 {display: none;}
+                        .viewerBadge_container__1QSob {display: none;}
                         </style>
                         """
 st.markdown(hide_streamlit_badge, unsafe_allow_html=True)
