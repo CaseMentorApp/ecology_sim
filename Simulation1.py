cfdcfa7175b9cf6cabb1ba1b8bca9aeb29263fab
CaseMentorApp <159547512+CaseMentorApp@users.noreply.github.com>
2024-02-10 23:49:51 +0800

    Update Simulation1.py

--- Simulation1.py
+++ Simulation1.py
@@ -46,26 +46,26 @@ with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 # defining the login information
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
+#authenticator = stauth.Authenticate(
+ #   config['credentials'],
+  #  config['cookie']['name'],
+   # config['cookie']['key'],
+    #config['cookie']['expiry_days'],
+    #config['preauthorized']
+#)
 
 
 
 
 # login authentication
-name, authentication_status, username = authenticator.login('Login', 'main')
-if username == 'password_hasher':
-        unhashed_password = st.text_input('password')
-        hashed_passwords = stauth.Hasher([unhashed_password]).generate()
-        st.write(hashed_passwords)
-        authenticator.logout('Log Out','main')
+#name, authentication_status, username = authenticator.login('Login', 'main')
+#if username == 'password_hasher':
+        #unhashed_password = st.text_input('password')
+        #hashed_passwords = stauth.Hasher([unhashed_password]).generate()
+        #st.write(hashed_passwords)
+        #authenticator.logout('Log Out','main')
  
-elif authentication_status and username != 'password_hasher':
+#elif authentication_status and username != 'password_hasher':
     #styling the buttons
 
     # creating a function to cache the read excel data
